b926eb169425751fbd8879d078e42ec046552416
Chanwoo <overelectric@gmail.com>
2020-05-02 10:18:29 +0900

    2020-05-02

--- springedu/src/main/java/vo/TeamVO.java
+++ springedu/src/main/java/vo/TeamVO.java
@@ -1,38 +1,38 @@
-//package vo;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//import javax.xml.bind.annotation.XmlRootElement;
-//
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//@XmlRootElement(name="GITVT")
-//public class TeamVO {
-//	private String teamName;
-//	private List<TeamMemberVO> teamMember;
-//	
-//	public TeamVO() {
-//		
-//	}
-//	
-//	TeamVO(String teamName, List<TeamMemberVO> teamMember) {
-//		this.teamName = teamName;
-//		this.teamMember = teamMember;
-//	}
-//	
-//	public String getTeamName() {
-//		return teamName;
-//	}
-//	public void setTeamName(String teamName) {
-//		this.teamName = teamName;
-//	}
-//	public List<TeamMemberVO> getTeamMember() {
-//		return teamMember;
-//	}
-//	public void setTeamMember(List<TeamMemberVO> teamMember) {
-//		this.teamMember = teamMember;
-//	}
-//	
-//}
+package vo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+@XmlRootElement(name="GITVT")
+public class TeamVO {
+	private String teamName;
+	private List<TeamMemberVO> teamMember;
+	
+	public TeamVO() {
+		
+	}
+	
+	TeamVO(String teamName, List<TeamMemberVO> teamMember) {
+		this.teamName = teamName;
+		this.teamMember = teamMember;
+	}
+	
+	public String getTeamName() {
+		return teamName;
+	}
+	public void setTeamName(String teamName) {
+		this.teamName = teamName;
+	}
+	public List<TeamMemberVO> getTeamMember() {
+		return teamMember;
+	}
+	public void setTeamMember(List<TeamMemberVO> teamMember) {
+		this.teamMember = teamMember;
+	}
+	
+}
